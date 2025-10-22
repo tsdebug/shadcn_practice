b58e34019eb39ef11fafd9525a8a37028d58bea0
TSdebug <taworkspace01@gmail.com>
2025-10-22 23:22:28 +0530

    Created a my route

---
+++ src/app/my/page.tsx
@@ -0,0 +1,10 @@
+function MyPage() {
+  return (
+    <main className="h-full flex flex-col justify-center items-center">
+      <div>MyPage</div>
+      <button className="px-6 py-2 hover:bg-blue-500 rounded my-3 bg-blue-800 ">Test Button</button>
+    </main>
+  )
+}
+
+export default MyPage
