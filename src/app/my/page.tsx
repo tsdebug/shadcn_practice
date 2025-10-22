d1886212d5b1493c940d120e31edc98181b7790b
TSdebug <taworkspace01@gmail.com>
2025-10-23 00:13:01 +0530

    Added a shadcn button

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function MyPage() {
   return (
     <main className="h-full flex flex-col justify-center items-center">
       <div>MyPage</div>
       <button className="px-6 py-2 hover:bg-blue-500 rounded my-3 bg-blue-800 ">Test Button</button>
+      <Button variant="outline">ShadCN button</Button>
     </main>
   )
 }
